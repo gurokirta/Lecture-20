bf093bcae6858cd1064e308c825118e0422fe285
Guro <70147494+gurokirta@users.noreply.github.com>
2023-01-25 13:23:16 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,87 @@
+"use strict";
+
+// Task #1
+// var user_array = [];
+
+// for (var i = 0; i < 100; i++) {
+//   user_array.push(i);
+//   if (user_array[i] % 2 === 1) {
+//     console.log(
+//       `Index of odd number is ${i} and the odd number itself is ${user_array[i]}`
+//     );
+//   }
+// }
+
+// console.log(user_array);
+
+// Task #2
+
+// var randomArray = [];
+
+// for (var i = 0; i < 50; i++) {
+//   randomArray.push(Math.round(Math.random() * 400 + 100));
+//   console.log(`${i} this is index of this number ${randomArray[i]}`);
+// }
+
+// Task #3
+
+// var user_name_array = [
+//   "guro",
+//   "dato",
+//   "avto",
+//   "giorgi",
+//   "erekle",
+//   "Genadi",
+//   "geno",
+//   "Gvanca",
+//   "giuli",
+//   "leqso",
+// ];
+
+// for (var i = 0; i < user_name_array.length; i++) {
+//   if (user_name_array[i][0].toLocaleLowerCase() === "g") {
+//     console.log(user_name_array[i]);
+//   }
+// }
+
+// Task #4
+
+var number_array = [2, 121, 4341, 242, 562, 43, 182, 55, 32, 98, 326]; //11
+var counter = 0;
+
+for (var i = 0; i <= number_array[i]; i++) {
+  if (number_array[i] % i === 0) {
+    console.log(number_array[i]);
+    // console.log(`item index ${i} and number ${number_array[i]} `);
+  }
+  //   console.log(number_array[i] + number_array[i]);
+}
+
+// console.log(counter);
+// array1.forEach(element => console.log(element));
+
+// Task #5
+// var randomArray = [];
+// var sum = 0;
+
+// for (var i = 0; i < 10; i++) {
+//   randomArray.push(Math.round(Math.random() * 25 + 1));
+//   sum += randomArray[i];
+// }
+// console.log(randomArray);
+// console.log(sum);
+
+// Task #6
+// var randomArray = [];
+// var highestNum = 0;
+
+// for (var i = 0; i < 10; i++) {
+//   randomArray.push(Math.round(Math.random() * 25 + 1));
+
+//   if (randomArray[i] > highestNum) {
+//     highestNum = randomArray[i];
+//   }
+// }
+
+// console.log(randomArray);
+// console.log(highestNum);
